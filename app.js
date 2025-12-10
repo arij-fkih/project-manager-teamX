031ea98609254c8b6b51ede856e79f3663a2be20
utilisateur2 <utilisateur2@email.com>
2025-12-10 12:30:23 +0100

    ajout fonction de suppresion

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ function creerProjet(nom,description){
         taches:[]
     };
 }
+
 //Exemple d'utilisation
 const projetDemo=creerProjet("ProjetDémo","Projet de test pour ProjectManager");
 console.log("Projet créé:",projectDemo);
+
+functionsupprimerProjet(listeProjets,idProjet){
+    return listeProjets.filter(projet=>projet.id!==idProjet);
+}
+//Exemple d'utilisation
+const nouvelleListe = supprimerProjet(projets,1);
+console.log("Liste aprés suppresion :",nouvelleListe);
