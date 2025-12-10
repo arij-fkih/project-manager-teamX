9397f644a01229b51cf61e9a58f7cb1b72e19eaa
utilisateur2 <utilisateur2@email.com>
2025-12-10 11:29:33 +0100

    Ajout de la fonctionnalité de création de projet

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("ProjectManager demarre ....")
+function creerProjet(nom,description){
+    return{
+        id:Date.now(),
+        nom:nom,
+        description:description,
+        taches:[]
+    };
+}
+//Exemple d'utilisation
+const projetDemo=creerProjet("ProjetDémo","Projet de test pour ProjectManager");
+console.log("Projet créé:",projectDemo);
